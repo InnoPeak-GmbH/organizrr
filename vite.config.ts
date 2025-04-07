420406f9a4df7407bdc30e3ebdadb651b537a1e4
RaviAnand Mohabir <moravrav@gmail.com>
2025-04-07 18:27:31 +0200

    chore: :wrench: increase workbox.maximumFileSizeToCacheInBytes to 18MB

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       registerType: "autoUpdate",
       includeAssets: ["favicon.ico", "apple-touch-icon.png"],
       workbox: {
-        maximumFileSizeToCacheInBytes: 17000000,
+        maximumFileSizeToCacheInBytes: 18000000,
       },
       manifest: {
         name: "Organizrr",
